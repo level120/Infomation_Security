171ebf5f1187acac6c17db40a874249746b73cd7
Gusam Park <tnara9573@gmail.com>
2017-11-06 10:54:31 +0900

    add unlimited tab

--- js/func.js
+++ js/func.js
@@ -498,6 +498,13 @@ function reset() {
     cipher = '';
 }
 
+function reset() {
+    document.getElementById("key").value = document.getElementById("text_encrypt1").value = document.getElementById("text_decrypt1").value = '';
+    document.getElementById("key").parentElement.parentElement.setAttribute('class', 'form-group has-feedback');
+    e = '';
+    cipher = '';
+}
+
 function copy_auto() {
     return new Promise(
         function (resolve, reject) {
@@ -551,3 +558,94 @@ function run() {
             return decryptText();
         });
 }
+
+function run2() {
+            var key = document.getElementById("key_2").value;
+
+            if (function() {
+                    return /^[0-9]+$/g.test(key) ? false : true;
+                }() || function() {
+                    key *= 1;
+                    if (key < 0 || key > 1023) return true;
+                    else return false;
+                }()) {
+                alert('키 값이 올바르지 않습니다.\n유효 키 값의 범위는 0 ~ 1023 입니다.');
+                return;
+            }
+
+            // Key
+            var varp = p(10, key, 10);
+            var shf1 = (ls(1, 5, varp >> 5) << 5) | ls(1, 5, varp & 0x1F);
+            var k1 = p(8, shf1, 10);
+            var shf2 = (ls(2, 5, shf1 >> 5) << 5) | ls(2, 5, shf1 & 0x1F);
+            var k2 = p(8, shf2, 10);
+
+            // encrypt
+            var text = document.getElementById("text_encrypt1_2").value;
+            var e = '';
+            var t = '';
+
+            for (var i in text) {
+                if (text[i].charCodeAt(0) > 0xFF) {
+                    var bitset = [text[i].charCodeAt(0) >> 8, text[i].charCodeAt(0) & 0xFF];
+                    var result = 0;
+
+                    for (var j in bitset) {
+                        var resIp = ip(false, bitset[j]);
+                        var resFk1 = fk(resIp, k1);
+                        var resSw = sw(resFk1);
+                        var resFk2 = fk(resSw, k2);
+                        var resIpInv = ip(true, resFk2);
+                        result |= resIpInv;
+
+                        if (j == 0) {
+                            result = result << 8;
+                        }
+                    }
+                    e += String.fromCharCode(result);
+                    document.getElementById("text_encrypt2_2").value = e;
+                } else {
+                    var resIp = ip(false, text[i].charCodeAt(0));
+                    var resFk1 = fk(resIp, k1);
+                    var resSw = sw(resFk1);
+                    var resFk2 = fk(resSw, k2);
+                    var resIpInv = ip(true, resFk2);
+                    e += String.fromCharCode(resIpInv);
+                    document.getElementById("text_encrypt2_2").value = e;
+                }
+            }
+
+            document.getElementById("text_decrypt2_2").value = e;
+
+            // decrypt
+            for (var i in e) {
+                if (e[i].charCodeAt(0) > 0xFF) {
+                    var bitset = [e[i].charCodeAt(0) >> 8, e[i].charCodeAt(0) & 0xFF];
+                    var result = 0;
+
+                    for (var j in bitset) {
+                        var resIp = ip(false, bitset[j]);
+                        var resFk1 = fk(resIp, k2);
+                        var resSw = sw(resFk1);
+                        var resFk2 = fk(resSw, k1);
+                        var resIpInv = ip(true, resFk2);
+                        result |= resIpInv;
+
+                        if (j == 0) {
+                            result = result << 8;
+                        }
+                    }
+                    t += String.fromCharCode(result);
+                    document.getElementById("text_decrypt1_2").value = t;
+                } else {
+                    var resIp = ip(false, e[i].charCodeAt(0));
+                    var resFk1 = fk(resIp, k2);
+                    var resSw = sw(resFk1);
+                    var resFk2 = fk(resSw, k1);
+                    var resIpInv = ip(true, resFk2);
+                    t += String.fromCharCode(resIpInv);
+
+                    document.getElementById("text_decrypt1_2").value = t;
+                }
+            }
+        }
